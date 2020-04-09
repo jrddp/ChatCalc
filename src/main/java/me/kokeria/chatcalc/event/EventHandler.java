614177f84c5c1ded80f049a7f06340f18985806d
Jared Peters <jar3dpt@gmail.com>
2020-04-09 03:51:25 -0400

    Add functionality

--- src/main/java/me/kokeria/chatcalc/event/EventHandler.java
+++ src/main/java/me/kokeria/chatcalc/event/EventHandler.java
@@ -11,11 +11,50 @@ public class EventHandler {
 
     protected static boolean runExpression(GuiChat chat) {
 
+        return runExprReplace(chat) || runExprAdd(chat);
+
+    }
+
+    private static boolean runExprReplace(GuiChat chat) {
+
+        GuiTextField field = chat.inputField;
+        String originalText = field.getText();
+        int cursor = field.getCursorPosition();
+
         try {
-            double solution = MathEngine.eval(chat.inputField.getText());
-            String solutionStr = format.format(solution);
-            chat.inputField.setText(solutionStr);
+            String word = ChatHelper.getWord(originalText, cursor);
+            double solution = MathEngine.eval(word);
+            String solStr = df.format(solution);
+            String newText = ChatHelper.replaceWord(originalText, cursor, solStr);
+            field.setText(newText);
             return true;
+        } catch (IndexOutOfBoundsException e) {
+            e.printStackTrace();
+            return false;
+        } catch (RuntimeException e) {
+            return false;
+        }
+
+    }
+
+    private static boolean runExprAdd(GuiChat chat) {
+
+        GuiTextField field = chat.inputField;
+        String originalText = field.getText();
+        int cursor = field.getCursorPosition();
+
+        try {
+            String word = ChatHelper.getWord(originalText, cursor);
+            if (!word.endsWith("=")) return false;
+            word = word.substring(0, word.length() - 1);
+            double solution = MathEngine.eval(word);
+            String solStr = df.format(solution);
+            String newText = ChatHelper.addWordAfterIndex(originalText, ChatHelper.getEndOfWord(originalText, cursor), solStr);
+            field.setText(newText);
+            return true;
+        } catch (IndexOutOfBoundsException e) {
+            e.printStackTrace();
+            return false;
         } catch (RuntimeException e) {
             return false;
         }
