717d98ee089874c5c74e01ef30c3fa5bd05ff391
Jared Peters <jar3dpt@gmail.com>
2020-04-09 02:51:10 -0400

    Basic outline for chat implementation

--- src/main/java/me/kokeria/chatcalc/event/EventHandler.java
+++ src/main/java/me/kokeria/chatcalc/event/EventHandler.java
@@ -1,4 +1,25 @@
 package me.kokeria.chatcalc.event;
 
+import me.kokeria.chatcalc.util.MathEngine;
+import net.minecraft.client.gui.GuiChat;
+
+import java.text.DecimalFormat;
+
 public class EventHandler {
+
+    private static final DecimalFormat format = new DecimalFormat("0.##");
+
+    protected static boolean runExpression(GuiChat chat) {
+
+        try {
+            double solution = MathEngine.eval(chat.inputField.getText());
+            String solutionStr = format.format(solution);
+            chat.inputField.setText(solutionStr);
+            return true;
+        } catch (RuntimeException e) {
+            return false;
+        }
+
+    }
+
 }
--- src/main/java/me/kokeria/chatcalc/event/EventListener.java
+++ src/main/java/me/kokeria/chatcalc/event/EventListener.java
@@ -1,11 +1,12 @@
 package me.kokeria.chatcalc.event;
 
-import me.kokeria.chatcalc.MathEngine;
-import net.minecraft.client.Minecraft;
+import me.kokeria.chatcalc.util.MathEngine;
 import net.minecraft.client.gui.GuiChat;
 import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.lwjgl.input.Keyboard;
+
+import java.text.DecimalFormat;
 
 public class EventListener {
 
@@ -13,14 +14,8 @@ public class EventListener {
     public void onGuiInput(GuiScreenEvent.KeyboardInputEvent event) {
         if (event.gui instanceof GuiChat && Keyboard.getEventKey() == Keyboard.KEY_TAB && Keyboard.getEventKeyState() && event.isCancelable()) {
 
-            GuiChat chat = (GuiChat) event.gui;
+            if (EventHandler.runExpression((GuiChat) event.gui)) event.setCanceled(true);
 
-            try {
-                MathEngine.eval(chat.inputField.getText());
-                System.out.println("Successful - " + event.isCancelable());
-            } catch (RuntimeException e) {
-                System.out.println("Not successful");
-            }
         }
     }
 
