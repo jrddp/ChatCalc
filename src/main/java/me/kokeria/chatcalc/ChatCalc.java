c96779dd27e1a48096d56deefbdb8fcca5c9c4d6
Jared Peters <jar3dpt@gmail.com>
2020-04-09 01:28:52 -0400

    Initial commit

---
+++ src/main/java/me/kokeria/chatcalc/ChatCalc.java
@@ -0,0 +1,19 @@
+package me.kokeria.chatcalc;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = ChatCalc.MODID, version = ChatCalc.VERSION)
+public class ChatCalc
+{
+    public static final String MODID = "chatcalc";
+    public static final String VERSION = "2.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+}
